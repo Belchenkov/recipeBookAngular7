61bd76776b9b240e7c55a05d66dce0bcb67ae8ad
Aleksey Belchenkov <u608110@gmail.com>
2019-05-05 16:08:54 +0300

    Rewrite with Services

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,41 @@
+import {EventEmitter, Injectable} from '@angular/core';
+
+import {Ingredient} from '../shared/ingredient.model';
+import {Recipe} from './recipe.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply Recipe',
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe(
+      'A Another Recipe',
+      'This is second Recipe',
+      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2016/12/4/0/WU1504H_lighter-asian-noodle-salad_s4x3.jpg.rend.hgtvcom.826.620.suffix/1480899697405.jpeg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
+    )
+  ];
+
+  constructor (private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
