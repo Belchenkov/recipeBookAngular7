4f30c3ccc5576edc0e163c6c0d383b86a96c2a14
Aleksey Belchenkov <u608110@gmail.com>
2020-05-25 10:20:51 +0300

    Lazy Loading: Auth, Shopping List

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -16,7 +16,7 @@ import { SharedModule } from '../shared/shared.module';
     ReactiveFormsModule,
     FormsModule,
     RouterModule.forChild([
-      { path: 'shopping-list', component: ShoppingListComponent }
+      { path: '', component: ShoppingListComponent }
     ])
   ]
 })
